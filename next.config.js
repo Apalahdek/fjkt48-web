c62b1ebad1db64bf3df9e2d5d8eaa37d4b62a9ac
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-10-23 23:26:19 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
         port: "",
-        pathname: "/v0/b/fjkt48-5bc17.appspot.com/o/**",
+        pathname: "/v0/b/jkt48-live.appspot.com/o/**",
       },
     ],
   },
